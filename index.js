4a31b13f35086055daf9f2e5a8da92a073c7a2b7
raghav13901 <63222090+raghav13901@users.noreply.github.com>
2020-05-06 12:35:08 +0530

    2ND version Reset Button

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-function toss(){
-var randomNumber1 = Math.ceil(Math.random()*6);
-document.querySelector(".img1").src = "images/dice"+randomNumber1 +".png";
-var randomNumber2 = Math.ceil(Math.random()*6);
-document.querySelector(".img2").src = "images/dice"+randomNumber2 +".png";
-if(randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML = "Draw! Toss again";
-}else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🎊 Player 1 Wins!"
-}else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🎊"
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+function toss() {
+  if (document.getElementById("p1").value == "" || document.getElementById("p2").value == "") {
+    document.querySelector("h2").innerHTML="Add Name Please!!";
+    document.querySelector("h2").style.color="#c3030a";
+  }
+  else {
+    if (p1.value == p2.value) {
+      document.querySelector("h2").innerHTML="Both Names are same, Please change the name";
+      document.querySelector("h2").style.color="#c3030a";
+    }else{
+    document.getElementById("pn1").innerHTML = p1.value;
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("pn2").innerHTML = p2.value;
+    document.getElementById("p2").style.display = "none";
+    document.querySelector("h2").style.color="#4ECCA3";
+    var randomNumber1 = Math.ceil(Math.random() * 6);
+    document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+    var randomNumber2 = Math.ceil(Math.random() * 6);
+    document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+    if (randomNumber1 === randomNumber2) {
+      document.querySelector("h2").innerHTML = "Draw! Toss again";
+    } else if (randomNumber1 > randomNumber2) {
+      document.querySelector("h2").innerHTML = p1.value + " Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+      document.querySelector("h2").innerHTML = p2.value + " Wins! ";
+    }
+    document.getElementById("Resetbtn").style.display="inline";
+  }
+  }
 }
+
+function ResetName() {
+  p1.value="";
+  p2.value="";
+  document.querySelector("h2").innerHTML = "Enter Your Name , Then Click Toss";
+  document.getElementById("pn1").innerHTML = p1.value;
+  document.getElementById("pn2").innerHTML = p2.value;
+  document.getElementById("p1").style.display = "block";
+  document.getElementById("p2").style.display = "block";
+  document.querySelector(".img1").src = "images/dice6.png";
+  document.querySelector(".img2").src = "images/dice6.png";
+  document.getElementById("Resetbtn").style.display = "none";
 }
