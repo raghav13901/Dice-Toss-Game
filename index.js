8d6297245231cc59537dbac31ff6c6af885d3877
raghav13901 <63222090+raghav13901@users.noreply.github.com>
2020-05-12 10:38:55 +0530

    5TH VERSION DARK MODE

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ var p2 = document.getElementById("p2");
 function toss() {
   if (document.getElementById("p1").value == "" || document.getElementById("p2").value == "") {
     document.querySelector("h2").innerHTML="Add Name Please!!";
-    document.querySelector("h2").style.color="#c3030a";
   }
   else {
     if (p1.value == p2.value) {
       document.querySelector("h2").innerHTML="Both Names are same, Please change the name";
-      document.querySelector("h2").style.color="#c3030a";
     }else{
     document.getElementById("pn1").innerHTML = p1.value;
     document.getElementById("p1").style.display = "none";
     document.getElementById("pn2").innerHTML = p2.value;
     document.getElementById("p2").style.display = "none";
-    document.querySelector("h2").style.color="#4ECCA3";
+    // document.querySelector("h2").style.color="#4ECCA3";
     var randomNumber1 = Math.ceil(Math.random() * 6);
     document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
     var randomNumber2 = Math.ceil(Math.random() * 6);
@@ -42,4 +40,19 @@ function ResetName() {
   document.querySelector(".img1").src = "images/dice6.png";
   document.querySelector(".img2").src = "images/dice6.png";
   document.getElementById("Resetbtn").style.display = "none";
+}
+function darkMode(){
+  document.querySelector("body").classList.toggle("light");
+  document.querySelector("body").classList.toggle("dark");
+  document.querySelectorAll("button")[0].classList.toggle("light");
+  document.querySelectorAll("button")[0].classList.toggle("dark");
+  document.querySelectorAll("button")[1].classList.toggle("light");
+  document.querySelectorAll("button")[1].classList.toggle("dark");
+  document.querySelectorAll("input")[1].classList.toggle("light");
+  document.querySelectorAll("input")[1].classList.toggle("dark");
+  document.querySelectorAll("input")[2].classList.toggle("light");
+  document.querySelectorAll("input")[2].classList.toggle("dark");
+  document.querySelector("footer").classList.toggle("light");
+  document.querySelector("footer").classList.toggle("dark");
+
 }
